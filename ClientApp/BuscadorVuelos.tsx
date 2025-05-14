51f18607e77e7652def80df546c74f948afbf234
GiovnniSalazar <giovanni.salazargonzalez.gen23@cbtis105.edu.mx>
2025-05-14 10:06:49 -0600

    lo que sea

---
+++ ClientApp/BuscadorVuelos.tsx
@@ -0,0 +1,18 @@
+const BuscadorVuelos = () => {
+    return (
+        <>
+            <div className="h1">Buscador de Vuelos</div>
+            <div className="card mt-4"></div>
+            <div className="card-header">Busqueda de vuelos</div>
+            <div className="card-body">
+                <div className="row">
+                <div className="col-sm-6"></div>
+            </div>
+        </div >
+    </div >
+
+        </>
+    )
+}
+
+export default BuscadorVuelos;
---
+++ ClientApp/components.ts
@@ -0,0 +1,8 @@
+import BuscadorVuelos from "./BuscadorVuelos";
+import MisDatos from "./MisDatos";
+
+export const components: { [key: string]: React.FC<any> } = {
+    MisDatos,
+    BuscadorVuelos
+
+};
