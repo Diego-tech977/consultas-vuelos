f6832f1ab110b526e007a2a1bbf37bf4d5567a38
GiovnniSalazar <giovanni.salazargonzalez.gen23@cbtis105.edu.mx>
2025-05-19 08:27:59 -0600

    si

--- ClientApp/BuscadorVuelos.tsx
+++ ClientApp/BuscadorVuelos.tsx
@@ -3,14 +3,18 @@ const BuscadorVuelos = () => {
         <>
             <div className="h1">Buscador de Vuelos</div>
             <div className="card mt-4"></div>
-            <div className="card-header">Busqueda de vuelos</div>
-            <div className="card-body">
-                <div className="row">
-                <div className="col-sm-6"></div>
+                <div className="card-header">Búsqueda de vuelos</div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-6"></div>
+                            <div className="mb-3">
+                                <label>Fecha Inicial</label>
+                                <input type="date" className="for-control" />
+                            </div>
+                        </div >
+                    </div >
+                </div >
             </div>
-        </div >
-    </div >
-
         </>
     )
 }
